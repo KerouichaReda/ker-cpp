e36a3ed84cb5a196a87ba8dcc1f76f0d74b928b1
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2023-08-11 16:28:37 +0800

    Update ordered_set.h

--- ker/container/ordered_set.h
+++ ker/container/ordered_set.h
@@ -5,7 +5,7 @@
 namespace ker {
 template <class T>
 class linear_set {
-    linear_set(std::size_t capacity) : capacity_(capacity) {}
+    linear_set(const std::size_t capacity,const  std::size_t eviction_size) : capacity_(capacity), eviction_size_(eviction_size) {}
     ~linear_set() {}
     void insert(const T& value) {
         std::set<T>::iterator itr = set_.insert(value);
@@ -14,17 +14,18 @@ class linear_set {
             evict();
         }
     }
-    bool contains(const T& value) { return set_.find(value) == set_.end(); }
+    bool contains(const T& value) { return set_.find(value) != set_.end(); }
 
    private:
     void evict() {
         std::set<T>::iterator itr = list_.front();
-        set_.erase(itr);
-        list_.pop_front();
+        set_.erase(list_.front(), list_.front() + eviction_size_);
+        list_.erase(list_.begin(), list_.begin() + eviction_size_);
     }
     std::set<T> set_;
     std::list<std::set<T>::iterator> list_;
-    std::size_t capacity_;
+    const std::size_t eviction_size_;
+    const std::size_t capacity_;
 }
 }
 
