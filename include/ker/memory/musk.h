6e0b80782ca9a69cc26725f06a07734e8e888b2d
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2024-04-24 16:35:06 +0800

    Update musk.h

---
+++ include/ker/memory/musk.h
@@ -0,0 +1,50 @@
+/// @file musk.h
+/// @author Reda KEROUCIHA <kerouicha_reda@outlook.com>
+/// @section LICENSE
+/// MIT License
+///
+/// Copyright (c) 2024 Reda Kerouicha <kerouicha_reda@outlook.com>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to
+/// deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE
+/// SOFTWARE.
+///
+
+namespace ker {
+class musk {
+   public:
+    musk() noexcept { secrete(); };
+    musk(int) noexcept { secrete(); };
+    musk(musk&&) noexcept { secrete(); };
+    musk(const musk&) noexcept { secrete(); };
+    ~musk() noexcept { secrete(); }
+    musk& operator=(const musk&) noexcept {
+        secrete();
+        return *this;
+    }
+    musk& operator=(musk&&) noexcept {
+        secrete();
+        return *this;
+    }
+
+   private:
+    void secrete() noexcept {}
+};
+}  // namespace ker
