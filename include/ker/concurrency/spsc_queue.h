c08850d271fa8f32268d867c3ff14481bc90c3ca
KEROUICHA_REDA <kerouicha.reda@gmail.com>
2025-04-09 10:39:00 +0800

    Update spsc_queue.h and btree.h

---
+++ include/ker/concurrency/spsc_queue.h
@@ -0,0 +1,44 @@
+/// @spsc_queue.h
+/// @author Reda KEROUCIHA <kerouicha_reda@outlook.com>
+/// @section LICENSE
+/// @brief  this file contain the definition of the database buffer
+/// MIT License
+///
+/// Copyright (c) 2024-present Reda Kerouicha <kerouicha_reda@outlook.com>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to
+/// deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE
+/// SOFTWARE.
+/// https://www.youtube.com/watch?v=K3P_Lmq6pw0
+#ifndef KER_CONCURENCY_SPSC_QUEUE_H
+#define KER_CONCURENCY_SPSC_QUEUE_H
+namespace ker {
+namespace concurency {
+class spsc_queue {
+   public:
+    spsc_queue();
+
+   private:
+};
+
+spsc_queue::spsc_queue() {}
+}   // namespace concurency 
+}   // namespace ker
+#endif  //! KER_SPSC_QUEUE_H
---
+++ include/ker/container/btree.h
@@ -0,0 +1,42 @@
+/// @btree.hpp
+/// @author Reda KEROUCIHA <kerouicha_reda@outlook.com>
+/// @section LICENSE
+/// @brief  this file contain the definition of the database buffer
+/// MIT License
+///
+/// Copyright (c) 2024-present Reda Kerouicha <kerouicha_reda@outlook.com>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to
+/// deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE
+/// SOFTWARE.
+///
+#ifndef KER_CONTAINER_BTREE_HPP
+#define KER_CONTAINER_BTREE_HPP
+namespace ker {
+class btree {
+   public:
+    btree();
+
+   private:
+};
+
+btree::btree() {}
+}  // namespace ker
+#endif  //! KER_CONTAINER_BTREE_HPP
